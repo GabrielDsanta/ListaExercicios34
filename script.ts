bf2525d3d9bb0ae22b8da00e17ce0a3e5386f57d
GabrielDSanta <eloviveiros@hotmail.com>
2022-10-10 20:32:19 -0300

    Exercicio 01 Finalizado

---
+++ script.ts
@@ -0,0 +1,27 @@
+
+
+
+//Exercicio 01//
+
+// let PerguntaA = String(prompt("Digite a priumeira palavra"))
+// let PerguntaB = String(prompt("Digite a segunda palavra"))
+
+// VerificarFinalStrings(PerguntaA, PerguntaB)
+
+// function VerificarFinalStrings(PerguntaA: string, PerguntaB: string){
+//     let TresUltimasLetrasA = PerguntaA.substring(PerguntaA.length - 3)
+//     let TresUltimasLetrasB = PerguntaB.substring(PerguntaA.length - 3)
+    
+//     const Resultado = (TresUltimasLetrasB == TresUltimasLetrasA) ? true : false
+    
+//     alert(Resultado)
+// }
+
+
+
+
+
+
+
+
+//Exercicio 02//
